09b70794159d650f5d1e77751bb16efb77d3fce8
Sightless21 <Firstsecondd45@gmail.com>
2024-08-09 22:53:00 +0700

    Create Borad

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function Square({value}) {
+  return(
+      <button className='square'>{value}</button>
   );
 }
 
-export default App;
+export default function Borad() {
+
+  const [square,SetSquare] = useState(Array(9).fill(null));
+  return(
+    <>
+      <div className='board-row'>
+        <Square value={square[0]}/>
+        <Square value={square[1]}/>
+        <Square value={square[3]}/>
+      </div>
+      <div className='board-row'>
+        <Square value={square[4]}/>
+        <Square value={square[5]}/>
+        <Square value={square[6]}/>
+      </div>
+      <div className='board-row'>
+        <Square value={square[7]}/>
+        <Square value={square[8]}/>
+        <Square value={square[9]}/>
+      </div>
+    </>
+  );
+}
