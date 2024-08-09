d08dbc4521f7b20c9e56097678ec690cb24911b1
Sightless21 <Firstsecondd45@gmail.com>
2024-08-09 23:44:39 +0700

    Tic-tac-toe No css

--- src/App.js
+++ src/App.js
@@ -14,9 +14,17 @@ export default function Borad() {
   const [xIsNext,setIsNext] = useState(true);
   const [squares,setSquare] = useState(Array(9).fill(null));
 
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   function handleClick(i) {
     //Check this squares clicked ?
-    if (squares[i]){
+    if (squares[i] || calculateWinner(squares)){
       return;
      }
     const nextSquares = squares.slice();
@@ -31,6 +39,7 @@ export default function Borad() {
   }
   return(
     <>
+    <div className="status">{status}</div>
       <div className='board-row'>
         <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
         <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
@@ -48,4 +57,24 @@ export default function Borad() {
       </div>
     </>
   );
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a,b,c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 }
